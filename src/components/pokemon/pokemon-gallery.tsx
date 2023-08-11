bdc02eb0ca6bbeec5ac7b71c31cd2331bb5c4865
ToEzBit <jirayut52328172@hotmail.com>
2023-08-11 21:40:34 +0700

    :lipstick: pokemon-gallery responsive

--- src/components/pokemon/pokemon-gallery.tsx
+++ src/components/pokemon/pokemon-gallery.tsx
@@ -8,7 +8,7 @@ export default function PokemonGallery() {
   const { pokemonList } = usePokemonStore();
 
   return (
-    <Grid columns={3} gap={{ base: 4, lg: 8 }}>
+    <Grid columns={{ base: 3, lg: 9 }} gap={{ base: 4, lg: 8 }}>
       {pokemonList.slice(0, 18).map((pokemonRes, idx) => (
         <PokemonItem key={idx} pokemonRes={pokemonRes} />
       ))}
